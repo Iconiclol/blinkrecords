fd6f941d800f57fdc6b6d9a25df14c01b541cd69
Iconiclol <101067401+Iconiclol@users.noreply.github.com>
2025-07-16 14:19:06 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'BLINK Records',
+  description: 'BLINK Records',
+  generator: 'BLINK Records',
 }
 
 export default function RootLayout({
